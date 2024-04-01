45b01fdaac52da772bc69bc7477778c9597f5aca
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-04-01 13:58:04 +0300

    Add examples of grammars

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use earley_parser::grammar::Grammar;
 use std::fs;
 
 fn main() {
-    let rules = fs::read_to_string("grammar").unwrap();
+    let rules = fs::read_to_string("grammar3").unwrap();
     let grammar = Grammar::from_rules(&rules).unwrap();
     println!("{}", grammar);
     println!();
-    grammar.parse("n+n*n");
+    // grammar.parse("n+n*n");
+    // grammar.parse("n+n"); //1
+    // grammar.parse("acb"); //2
+    // grammar.parse("aacbb"); //2
+    grammar.parse("1+(2*3-4)"); //3
 }
