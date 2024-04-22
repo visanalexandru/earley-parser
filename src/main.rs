8f500c215f586c6f921fc76309b7c84f155114c6
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-04-22 12:35:11 +0300

    Fix main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
+pub use earley_parser::grammar::parser::parse_tree::render_tree;
 use earley_parser::grammar::{write_tree_to_dot, Grammar};
 use std::fs;
-
 fn main() {
-    let rules = fs::read_to_string("grammar").unwrap();
+    let rules = fs::read_to_string("grammar-expression").unwrap();
     let grammar = Grammar::from_rules(&rules).unwrap();
     println!("{}", grammar);
     println!();
 
-    let trees = grammar.parse("(n+n+(n*n)-n/n)");
+    let trees = grammar.parse("n+n+n");
     println!("Got {} derivation trees", trees.len());
 
-    for tree in trees {
+    for (index, tree) in trees.iter().enumerate() {
         let mut to = String::new();
         write_tree_to_dot(&mut to, &tree).unwrap();
-        println!("{}", to);
-        println!();
+        let path = format!("tree_{}.svg", index);
+        render_tree(&tree, &path)?;
     }
 }
