b52da4761af1d632f48ff181e8d78b0bb5dbe0b2
visan <alexandruvisan44@yahoo.com>
2024-04-23 20:22:31 +0300

    Add stdin loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use earley_parser::grammar::{render_tree, write_tree_to_dot, Grammar};
 use std::fs;
+use std::io;
+
 fn main() {
-    let rules = fs::read_to_string("grammar-expression").unwrap();
+    let rules = fs::read_to_string("grammar").unwrap();
     let grammar = Grammar::from_rules(&rules).unwrap();
     println!("{}", grammar);
-    println!();
+    println!("Enter your words:");
 
-    let trees = grammar.parse("n+n+n");
-    println!("Got {} derivation trees", trees.len());
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
+        let trees = grammar.parse(line);
 
-    for (index, tree) in trees.iter().enumerate() {
-        let mut to = String::new();
-        write_tree_to_dot(&mut to, &tree).unwrap();
-        let path = format!("tree_{}.svg", index);
-        render_tree(&tree, &path).unwrap();
+        println!("Got {} derivation trees", trees.len());
+
+        for (index, tree) in trees.iter().enumerate() {
+            let mut to = String::new();
+            write_tree_to_dot(&mut to, &tree).unwrap();
+            let path = format!("tree_{}.svg", index);
+            render_tree(&tree, &path).unwrap();
+        }
     }
 }
